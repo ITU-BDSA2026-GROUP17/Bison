02aaca978b1d1e2babaea65a26360354856af858
Tobias <toov@itu.dk>
2026-09-02 14:06:21 +0200

    feat:add formatting requirements to ci
    Also adds a git hook for the devenv environment.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.CommandLine;
 using System.Globalization;
+
 using CsvHelper;
 using CsvHelper.Configuration;
 
--- devenv.nix
+++ devenv.nix
@@ -15,10 +15,24 @@
         '';
         packages = [ pkgs.watchexec ];
     };
-    
+
     # https://devenv.sh/basics/
     enterShell = '''';
-    
+
+    git-hooks.hooks.formatting = {
+        enable = true;
+        
+        # The name of the hook (appears on the report table):
+        name = "Formatting of .NET files";
+
+        # The command to execute (mandatory):
+        entry = "make format";
+        
+        # The pattern of files to run on (default: "" (all))
+        # see also https://pre-commit.com/#hooks-files
+        files = "\\.(cs)$";
+    };
+
     # https://devenv.sh/tests/
     # https://devenv.sh/services/
     # https://devenv.sh/tasks/
