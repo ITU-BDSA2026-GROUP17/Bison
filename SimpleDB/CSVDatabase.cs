95ba37127988c9e9a6d378a6bb354b62092bd8d1
Andreas Ebel Nielsen <aebn@itu.dk>
2026-09-03 13:28:45 +0200

    <refactor>: Created simple database for storing in CSV (#18)
    Created new library and files for database. Still need to implement database into main project file.
    
    Co-authored-by: Luca <lrec@itu.dk>
    Co-authored-by: Parker <pask@itu.dk>
    Co-authored-by: Toby <toov@itu.dk>

---
+++ SimpleDB/CSVDatabase.cs
@@ -0,0 +1,33 @@
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+namespace SimpleDB;
+public class CSVDatabase<T> : IDatabaseRepository<T>
+{
+    const string CSV_FILE_PATH = "bison_observe_cli_db.csv";
+
+     public IEnumerable<T> Read(int? limit = null)
+    {
+        using var reader = new StreamReader(CSV_FILE_PATH);
+        using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+        
+        var records = csv.GetRecords<T>();
+        
+        return records;
+    }
+    public void Store(T record)
+    {
+       var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            // Don't write the header again.
+            HasHeaderRecord = false,
+        };
+
+        using var stream = File.Open(CSV_FILE_PATH, FileMode.Append);
+        using var writer = new StreamWriter(stream);
+        using var csv = new CsvWriter(writer, config);
+
+        csv.WriteRecord(record);
+    }
+}
