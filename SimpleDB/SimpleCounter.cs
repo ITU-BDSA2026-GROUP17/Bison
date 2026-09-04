e51e7e9b63bc4e0ff8d6205c79ea363d2522cb86
Tobias <toov@itu.dk>
2026-09-04 13:46:19 +0200

    feat:simple counter
    A simple counter that saves the last given number to a file :)
    
    Co-authored-by: Magnus <mawb@itu.dk>
    Co-authored-by: Parker <pask@itu.dk>
    Co-authored-by: Luca <lrec@itu.dk>
    Co-authored-by: Andreas <aebn@itu.dk>

---
+++ SimpleDB/SimpleCounter.cs
@@ -0,0 +1,54 @@
+namespace Bison.SimpleDB;
+
+public class SimpleCounter
+{
+    readonly string _filePath;
+
+    public SimpleCounter(string filePath)
+    {
+        _filePath = filePath;
+
+        if (!File.Exists(_filePath))
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(_filePath));
+            using var writer = File.CreateText(_filePath);
+            writer.Write('-');
+            writer.Write('1');
+        }
+        else
+        {
+            var input = File.ReadLines(_filePath);
+            try
+            {
+                int.Parse(input.First());
+            }
+            catch
+            {
+                using var writer = File.CreateText(_filePath);
+                writer.Write('-');
+                writer.Write('1');
+            }
+        }
+    }
+
+    public int NextNumber()
+    {
+        var input = File.ReadLines(_filePath);
+        try
+        {
+            var nextId = int.Parse(input.First()) + 1;
+
+            using var writer = File.CreateText(_filePath);
+            writer.Write(nextId);
+
+            return nextId;
+        }
+        catch
+        {
+            using var writer = File.CreateText(_filePath);
+            writer.Write('0');
+
+            return 0;
+        }
+    }
+}
