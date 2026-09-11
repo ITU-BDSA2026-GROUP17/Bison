b3ca81226817fd0142a83736024c6b6c6c947673
Tobias <toov@itu.dk>
2026-09-11 11:44:53 +0200

    test:unix timestamp translation

---
+++ test/Bison.Utilities.Tests/UnixTimestampUnitTest.cs
@@ -0,0 +1,30 @@
+namespace Bison.CLI.Client.Tests;
+
+using Bison.Utilities;
+
+using FluentAssertions;
+
+using FsCheck.Xunit;
+
+public class UnixTimestampUnitTest
+{
+    static DateTime GetDateTimeWithoutSubseconds()
+    {
+        var current = DateTime.Now;
+        var ticks = current.Ticks / TimeSpan.TicksPerSecond;
+
+        DateTime without = new(ticks * TimeSpan.TicksPerSecond);
+        without.Nanosecond.Should().Be(0);
+        return without;
+    }
+
+    [Fact]
+    public void UnixTimestampTranslationWorks()
+    {
+        var current = GetDateTimeWithoutSubseconds();
+        var unix = DateTimeUtilities.DateTimeToUnixTimeStamp(current);
+        var restored = DateTimeUtilities.UnixTimeStampToDateTime(unix);
+
+        restored.Should().Be(current);
+    }
+}
