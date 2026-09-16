8efbcf569343d30e137c0ebac5b4560b66885e65
Tobias <toov@itu.dk>
2026-09-16 23:12:54 +0200

    fix:dev processes
    Makes sure that the DB instance will be bunded with the CLI on release
    Also updates devenv setup

--- devenv.nix
+++ devenv.nix
@@ -1,17 +1,24 @@
 { pkgs, ... }:
 
-{
-    languages.dotnet = {
-        enable = true;
-    };
+let
+  # Combine .NET 8 and .NET 10 environments 
+  my-dotnet-bundle = with pkgs.dotnetCorePackages; combinePackages [
+    sdk_8_0
+    sdk_10_0
+    aspnetcore_8_0
+  ];
+in {
     # https://devenv.sh/packages/
-    packages = with pkgs; [
-        dotnet-aspnetcore
+    packages = [
+        my-dotnet-bundle
+        pkgs.roslyn-ls
     ];
+    env.DOTNET_ROOT = "${my-dotnet-bundle}";
+    
     scripts.watcher = {
         exec = ''
             watchexec -c -e cs \
-            "cargo clippy && cargo test && cargo run"
+            "dotnet format && dotnet run --project src/Bison.Database"
         '';
         packages = [ pkgs.watchexec ];
     };
