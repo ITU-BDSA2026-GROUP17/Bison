0d07f4f95c35ccf1a760fcf348dfd2377b0bcecc
Tobias <toov@itu.dk>
2026-09-16 17:39:35 +0200

    fix:e2e cli test
    Makes sure the E2E test automatically runs the DB in the background.

--- test/Bison.CLI.Client.Tests/E2E.cs
+++ test/Bison.CLI.Client.Tests/E2E.cs
@@ -6,9 +6,34 @@ using FluentAssertions;
 
 public class E2E
 {
+    public static Process StartDatabase() {
+        using var process = Process.Start(new ProcessStartInfo()
+        {
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            FileName = "dotnet",
+            Arguments = "run --project src/Bison.Database",
+            WorkingDirectory = "../../../../../"
+        });
+        Assert.True(process is not null);
+
+        while (true)
+        {
+            var output = process.StandardOutput.ReadLine();
+            if (output == null || output.Contains("localhost"))
+            {
+                break;
+            }
+        }
+
+        return process;
+    }
     [Fact]
     public async Task CommentOnNonExistentObservationTest()
     {
+        var db = StartDatabase();
         using var process = Process.Start(new ProcessStartInfo()
         {
             UseShellExecute = false,
@@ -25,11 +50,13 @@ public class E2E
         await process.WaitForExitAsync();
 
         output.Trim().Should().Be("Observation id 2147483647 does not exist");
+        db.Close();
     }
 
     [Fact]
     public async Task TestObservationWorks()
     {
+        var db = StartDatabase();
         using var process = Process.Start(new ProcessStartInfo()
         {
             UseShellExecute = false,
@@ -46,11 +73,13 @@ public class E2E
         await process.WaitForExitAsync();
 
         output.Trim().Should().Be("Observation has been saved.");
+        db.Close();
     }
 
     [Fact]
     public async Task CommentOnExistentObservationWorks()
     {
+        var db = StartDatabase();
         using var process = Process.Start(new ProcessStartInfo()
         {
             UseShellExecute = false,
@@ -67,5 +96,6 @@ public class E2E
         await process.WaitForExitAsync();
 
         output.Trim().Should().Be("Comment has been saved.");
+        db.Close();
     }
 }
